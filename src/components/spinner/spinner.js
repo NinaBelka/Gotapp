7ab9f56b9f7089e2f7c15fe8ab8672ea3974af02
Nina <mysha76@mail.ru>
2020-08-13 14:24:52 +0300

    Обработана ошибка. Вставлен лоудер как отдельный компонент

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './spinner.css';
+
+const Spinner = () => {
+  return (
+    <div class="loadingio-spinner-spin-1yphk5rp82d">
+      <div class="ldio-hmqcc30pujm">
+        <div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Spinner;  
