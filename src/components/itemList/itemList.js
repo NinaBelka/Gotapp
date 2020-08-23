133d083f1b28587b4698fd72d594fed2f9e14c70
Nina <mysha76@mail.ru>
2020-08-23 14:08:30 +0300

    Использованы паттерны реакт

--- src/components/itemList/itemList.js
+++ src/components/itemList/itemList.js
@@ -1,69 +1,50 @@
 import React, { Component } from 'react';
 import './itemList.css';
-import ErrorMessage from '../errorMessage';
-import Spinner from '../spinner/';
-
+import Spinner from '../spinner';
 
 export default class ItemList extends Component {
 
   state = {
-    itemList: null,
-    error: false
+    itemList: null
   }
+
   componentDidMount() {
     const { getData } = this.props;
 
     getData()
       .then((itemList) => {
         this.setState({
-          itemList,
-          error: false
-        });
+          itemList
+        })
       })
-      .catch(() => { this.onError() });
   }
-  componentDidCatch() {
-    this.setState({
-      charList: null,
-      error: true
-    })
-  }
-  onError(status) {
-    this.setState({
-      charList: null,
-      error: true
-    })
-  }
+
   renderItems(arr) {
     return arr.map((item) => {
       const { id } = item;
+
       const label = this.props.renderItem(item);
 
       return (
         <li
           key={id}
           className="list-group-item"
-          onClick={() => this.props.onItemSelected(id)}
-        >
+          onClick={() => this.props.onItemSelected(id)}>
           {label}
         </li>
       )
     })
   }
 
-
   render() {
-    const { itemList, error } = this.state;
-
-    if (error) {
-      return <ErrorMessage />
-    }
+    const { itemList } = this.state;
 
     if (!itemList) {
       return <Spinner />
     }
 
     const items = this.renderItems(itemList);
+
 
     return (
       <ul className="item-list list-group">
